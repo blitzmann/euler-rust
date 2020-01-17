0e6d1a40701710ae4918e8ea3f4408b21a2caf71
blitzmann <holmes.ryan.90@gmail.com>
2020-01-17 09:03:40 -0500

    p11 more wip

--- src/p011.rs
+++ src/p011.rs
@@ -36,38 +36,29 @@ const GRID: &str = "\
     20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
     01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
 
-fn idxVert(i: usize, list: &Vec<u32>) -> Option<u32> {
-    fn test() {
-        
-    }
-    // let vals = vec![list[i]];
-    let thing: Option<Vec<_>> = (1..ARM_LENGTH)
+fn idxVert(i: usize, list: &Vec<u32>) -> u32 {
+    let values: Option<Vec<_>> = (1..ARM_LENGTH)
         .map(|x| {
-            let t = i as i32 - ( WIDTH * x) as i32;
+            let t = i as i32 - (WIDTH * x) as i32;
             if t < 0 {
                 None
             } else {
                 list.get(t as usize)
             }
         })
-        .collect();
+        .collect(); // collect into a single Option which may have a vec
 
-            if (i < 100){
-    println!("{:?}", thing);
-            }
-
-            match thing {
-                None => None,
-                Some(vec) => Some(vec.product())
-            }
- 
+    match values {
+        None => 0, // happens when out of bounds, so it's effectively 0
+        Some(vec) => vec.into_iter().product(), 
+    }
 }
 
 pub fn solve() -> u64 {
-    let list = GRID
+    let list:Vec<u32> = GRID
         .split_whitespace()
         .map(|c| c.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .collect();
 
     for (i, x) in list.iter().enumerate() {
         let results = idxVert(i, &list);
